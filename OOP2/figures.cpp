c2e410e45805d9e38480c863504033dc6b434513
noodzz <102947953+noodzz@users.noreply.github.com>
2022-09-17 17:33:28 +0500

    создан класс LIne

--- OOP2/figures.cpp
+++ OOP2/figures.cpp
@@ -14,23 +14,49 @@ public:
 	}
 	Point(int x, int y)
 	{
-		cout << "Point(int x, int y)" << endl;
 		this->x = x;
 		this->y = y;
+		cout << x << " " << y << " Point(int x, int y)" << endl;
+
 	}
 	Point(Point& p)
 	{
-		cout << "Point(Point &p)" << endl;
+	
 		x = p.x;
 		y = p.y;
+		cout << x << " " << y << " Point(Point &p)" << endl;
 	}
 	~Point()
 	{
-		cout << x << " " << y << endl;
-		cout << "~Point()" << endl;
+		cout << x << " " << y << " ~Point()" << endl;
 	}
 };
 
+class Line {
+private:
+	Point A;
+	Point B;
+public:
+	Line() : A(), B()
+	{
+		cout << "Line()" << endl;
+	}
+	Line(int x1, int y1, int x2, int y2)
+		:A(x1, y1), B(x2, y2)
+	{
+		cout << "Line(Point a, Point b)" << endl;
+
+	}
+	Line(Line& l)
+		:A(l.A), B(l.B)
+	{
+		cout << "Line(Line& l)" << endl;
+	}
+	~Line()
+	{
+		cout << "~Line()" << endl;
+	}
+};
 
 class Triangle {
 protected:
@@ -72,5 +98,10 @@ public:
 int main()
 {
 
+	Line l1(1, 2, 3, 4);
+	//Triangle* t1 = new Triangle();
+	//Triangle* t2 = new Triangle(*t1);
+	//delete t1;
+	//delete t2;
 	return 0;
 }
