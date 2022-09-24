313df25f373de96dd9e687b7e4365ea3ecb0f5bb
noodzz <102947953+noodzz@users.noreply.github.com>
2022-09-24 12:50:02 +0500

    добавлен метод для дочернего класса, вспомогательные методы

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -85,6 +85,15 @@ public:
 };
 
 class SquareMatrix : public Matrix {
+private:
+	SquareMatrix minor(int r, int c);
+	float AlgCompl(int r, int c)
+	{
+		int sign;
+		if ((r + c) % 2 == 0) sign = 1;
+		else sign = -1;
+		return sign * minor(r, c).determinant();
+	}
 public:
 	SquareMatrix() : Matrix() 
 	{
@@ -109,14 +118,54 @@ public:
 	{
 		cout << "~SquareMatrix()" << endl;
 	}
+	float determinant()
+	{
+		float det = 0;
+		if (rows == 1) det = vals[0][0];
+		else if (rows == 2)
+			det = vals[0][0] * vals[1][1] - vals[0][1] * vals[1][0];
+		else
+		{
+			for (int i = 0; i < rows; i++)
+				det += vals[0][i] * AlgCompl(1, i + 1);
+		}
+		return det;
+	}
 };
+
+SquareMatrix SquareMatrix::minor(int r, int c)
+{
+	if (r <= rows && c <= cols)
+	{
+		int _rows = 0, _cols = 0;
+		SquareMatrix tmp(rows - 1);
+		for (int i = 0; i < rows; i++)
+		{
+			if (i == r - 1) continue;
+			else {
+				for (int j = 0; j < rows; j++)
+				{
+					if (j == c - 1) continue;
+					else
+					{
+						tmp.vals[_rows][_cols] = vals[i][j];
+						_cols++;
+					}
+				}
+			}
+			_rows++;
+			_cols = 0;
+		}
+		return tmp;
+	}
+}
 
 int main()
 {
-	SquareMatrix m(2);
-	m.random_fill();
+	SquareMatrix m(3);
+	m.fill();
 	m.print();
-	m.transpose();
-	m.print();
+	cout << "Determinant = " << m.determinant() << endl;
+	
 
 }
